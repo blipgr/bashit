cad13582eb4830b21411a595841f903352fe0e32
root <admin@example.com>
2023-09-25 01:53:56 +0300

    сделал скрипт для устновки bashit

---
+++ bashit.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Проверяем, существует ли директория ~/.bash_it
+if [ -d "$HOME/.bash_it" ]; then
+	  # Если существует, выводим сообщение и выходим из скрипта
+	    echo "Bash-it уже установлен!"
+	      exit
+fi
+
+# Если не существует, клонируем Bash-It
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+echo "y" | ~/.bash_it/install.sh
+
+# Подключаем Bash-It
+source "$HOME/.bash_it/bash_it.sh"
+
+# Включаем Bash-it plugins для git и kubernetes
+bash-it enable completion git
+bash-it enable completion kubectl
+bash-it enable completion systemd
+
+# Включаем Bash-It aliases
+bash-it enable alias kubectl
+bash-it enable alias systemd
+bash-it enable alias git
+# Перезагружаем Bash-It
+bash-it reload
+
+# Подключаем Bash-It еще раз
+source "$HOME/.bash_it/bash_it.sh"
+
+# Клонируем и устанавливаем ble.sh
+git clone --recursive --depth=1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
+make -C ble.sh install PREFIX=~/.local
+echo 'source ~/.local/share/blesh/ble.sh' >> ~/.bashrc
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+Установка bashit в автомате
+
+curl -o bashit.sh -L https://github.com/blipgr/bashit.git/bashit.sh && chmod +x bashit.sh && ./bashit.sh  
+или
+wget https://github.com/blipgr/bashit.git/bashit.sh -O bashit.sh && chmod +x bashit.sh && ./bashit.sh  
